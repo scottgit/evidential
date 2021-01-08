cafac9295af6b4398319df6333b8e536a9d865a8
Scott Smith <scott.sncsmith@gmail.com>
2021-01-07 20:28:55 -0800

    Finished auth and user routes

--- app/forms/edit_user_form.py
+++ app/forms/edit_user_form.py
@@ -1,15 +1,17 @@
+from typing import Optional
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError, EqualTo, Length, Regexp
+from wtforms.validators import Optional, DataRequired, Email, ValidationError, EqualTo, Length, Regexp
+from app.models import User
 from .includes import user_exists
 
-class SignUpForm(FlaskForm):
-    first_name = StringField('firstName', validators=[DataRequired()])
-    last_name = StringField('lastName', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
+class EditUserForm(FlaskForm):
+    first_name = StringField('firstName', validators=[Optional()])
+    last_name = StringField('lastName', validators=[Optional()])
+    email = StringField('email', validators=[Optional(), Email(), user_exists])
     confirm_password = StringField('confirmPassword')
     password = StringField('password', validators=[
-        DataRequired(),
+        Optional(),
         Length(min=8, message='Password must be 8 or more characters'), EqualTo('confirmPassword', message='Confirmed password must match password'),
         Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])$", message='Password must contain 1 lower & 1 upper case letter, 1 number, and 1 special character of @$!%*?&')
         ])
